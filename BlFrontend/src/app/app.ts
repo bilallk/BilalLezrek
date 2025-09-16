691c11a648e1f48c9eb79fbc8d6b2386c6e49b52
Bilal Lezrek <Bilallk@live.be>
2025-09-16 09:50:58 +0200

    side nav: intial commit

--- BlFrontend/src/app/app.ts
+++ BlFrontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNav } from './side-nav/side-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SideNav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
